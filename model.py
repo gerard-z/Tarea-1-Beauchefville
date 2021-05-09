521688f60de94b2c655ba9f491d54b147647877f
Gerard Cathalifaud Salazar <gerard.an@hotmail.com>
2021-05-09 05:00:58 -0400

    Ideas por hacer en model

--- model.py
+++ model.py
@@ -3,3 +3,6 @@ import glfw
 import numpy as np
 import grafica.transformations as tr
 
+#### IDEA1: Agregar la "vacuna" con una textura y alguna forma de estrella que cambia dinámicamente sus vertices
+
+### Sign : 0.2 - 0.5 - 1
